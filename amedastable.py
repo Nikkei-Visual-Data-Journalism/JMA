1c7ea737f88eee94f33e48d7c56f24403c308da2
Kazuhiro Kida <55435750+kazuhirokida@users.noreply.github.com>
2023-07-13 12:52:12 +0900

    Create amedastable.py

---
+++ amedastable.py
@@ -0,0 +1,18 @@
+import requests
+import pandas as pd
+
+data = requests.get('https://www.jma.go.jp/bosai/amedas/const/amedastable.json').json()
+
+list_ = []
+for key in data.keys():
+  list_.append(data[key])
+
+amedas = pd.DataFrame(list_)
+
+amedas['code'] = data.keys()
+
+amedas.lat = [x[0] + x[1]/60 for x in amedas.lat]
+
+amedas.lon = [x[0] + x[1]/60 for x in amedas.lon]
+
+amedas.to_csv('amedastable.csv',index=False,encoding='utf-8-sig')
