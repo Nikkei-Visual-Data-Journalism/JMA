0b2eded08db46e4af46be12fa4b5df39f6b36a70
Kazuhiro Kida <55435750+kazuhirokida@users.noreply.github.com>
2023-07-28 17:18:06 +0900

    Update maxtem.py

--- maxtem.py
+++ maxtem.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
-df = pd.read_csv('https://www.data.jma.go.jp/stats/data/mdrr/tem_rct/alltable/mxtemsadext00_rct.csv',encoding='Shift-JIS',dtype='str')
+df = pd.read_csv('https://www.data.jma.go.jp/stats/data/mdrr/tem_rct/alltable/mxtemsadext00_rct.csv',
+                 encoding='Shift-JIS',
+                 dtype={'観測所番号':'str','今日の最高気温起時（時）':'str','今日の最高気温起時（分）':'str','今日の最高気温(℃)':'float'})
 
-amdno = pd.read_csv('https://github.com/Nikkei-Visual-Data-Journalism/JMA/raw/main/amedastable.csv',index_col='amdno')
-
-amdno.index = amdno.index.astype('str')
+amdno = pd.read_csv('https://github.com/Nikkei-Visual-Data-Journalism/JMA/raw/main/amedastable.csv',
+                    dtype={'amdno':'str','lat':'float','lon':'float'}).set_index('amdno')
 
 df['lat'] = df['観測所番号'].map(amdno['lat'].to_dict())
 
@@ -19,3 +20,5 @@ df = df.rename(columns={'今日の最高気温(℃)':'気温'})
 df[['観測所','lat','lon','時間','気温']].to_csv('maxtem.csv',index=False,encoding='utf-8-sig')
 
 df[df['今年最高'] == '1'].reset_index(drop=True)[['観測所','lat','lon','時間','気温']].to_csv('yearhigh.csv',index=False,encoding='utf-8-sig')
+
+df[df['気温'] >= 35].reset_index(drop=True)[['観測所','lat','lon','時間','気温']].to_csv('above35.csv',index=False,encoding='utf-8-sig')
